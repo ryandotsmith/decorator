46f498632962e970f1d743357e17999fe4070a24
ryandotsmith <this.ryansmith@gmail.com>
2009-11-03 20:50:20 -0600

    updated the item class with some examples

--- item.rb
+++ item.rb
@@ -1,25 +1,37 @@
+require 'Date'
+
 class Item
   attr_accessor :description, :completion_datetime
-  
+
+  def initialize 
+    @completion_datetime = Date.new
+    @description = "new todo item"
+  end
+
   def complete!
-    completion_datetime = DateTime.now 
+    @completion_datetime = Date.today
   end
 
 end
 
 class ItemWithEstimation < Item
   attr_accessor :estimated_time_to_completion
-
-  def itnitalize(estimated=0)
-    estimated_time_to_completion = estimated
+  
+  def initialize(estimated=0) 
+    super()
+    @estimated_time_to_completion = estimated
   end
 
   def log_work(hours)
-    estimated_time_to_completion -= hours
+    @estimated_time_to_completion -= hours
   end
 
 end
 
+
 item = ItemWithEstimation.new
+puts item.description
 item.complete!
-item.log_work(3)
+item.log_work(4)
+
+
