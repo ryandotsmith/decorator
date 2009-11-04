6d78097b85ebc1824584e9a1aa37162ed56a3df9
ryandotsmith <this.ryansmith@gmail.com>
2009-11-03 20:56:51 -0600

    did a class eval on item

--- item.rb
+++ item.rb
@@ -1,25 +1,29 @@
+require 'Date'
+
 class Item
   attr_accessor :description, :completion_datetime
   
   def complete!
-    completion_datetime = DateTime.now 
+    @completion_datetime = DateTime.now 
+    puts "complete!"
   end
 
 end
 
-class ItemWithEstimation < Item
+Item.class_eval do
   attr_accessor :estimated_time_to_completion
 
-  def itnitalize(estimated=0)
-    estimated_time_to_completion = estimated
+  def initialize(estimated=0)
+    @estimated_time_to_completion = estimated
   end
 
   def log_work(hours)
-    estimated_time_to_completion -= hours
+    @estimated_time_to_completion -= hours
+    puts "working...."
   end
 
 end
 
-item = ItemWithEstimation.new
+item = Item.new
 item.complete!
 item.log_work(3)
