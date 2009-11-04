30a5c356855a933a77470099b3c5daa07ada75d4
ryandotsmith <this.ryansmith@gmail.com>
2009-11-03 22:09:22 -0600

    added some meta hackery

--- item.rb
+++ item.rb
@@ -1,7 +1,55 @@
-require 'Date'
+require 'rubygems'
+require 'active_support/inflector'
+
+class Symbol
+  def to_class
+    Kernel.const_get(ActiveSupport::Inflector::camelize(self))
+  end
+end
+
+module Decorator
+  def initialize(decorated)
+    @decorated = decorated
+  end
+  def method_missing(method, *args)
+    args.empty? ? @decorated.send(method) : @decorated.send(method, args)
+  end
+end
+
+class Estimation
+  attr_accessor :estimated_time_to_completion
+  include Decorator  
+
+  def log_work(hours)
+    @estimated_time_to_completion -= hours
+    puts "working...."
+    return true
+  end
+
+end
+
+class Notifier
+  attr_accessor :message
+  include Decorator
+
+  def ping(person="@ryandotsmith")
+    puts "#{person}: #{@message}"
+  end
+
+end
+
+class DecoratedItem
+  include Decorator
+end
 
 class Item
   attr_accessor :description, :completion_datetime
+
+  def self.with(*extras)
+    extras << :decorated_item
+    extras.inject(self.new) { |memo, extra| memo = extra.to_class.new(memo) }
+  end
+
 
   def initialize 
     @completion_datetime = Date.new
@@ -15,25 +63,9 @@ class Item
 
 end
 
-class ItemWithEstimation < Item
-  attr_accessor :estimated_time_to_completion
-  
-  def initialize(estimated=0) 
-    super()
-    @estimated_time_to_completion = estimated
-  end
 
-  def log_work(hours)
-    @estimated_time_to_completion -= hours
-    puts "working...."
-  end
-
-end
-
-
-item = ItemWithEstimation.new
-puts item.description
-item.complete!
+item = Item.with :estimation, :notifier
+item.estimated_time_to_completion = 10
 item.log_work(4)
-
+item.ping
 
